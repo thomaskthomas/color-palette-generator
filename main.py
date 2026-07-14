6aba31b32551d149ebbf782af8643a330aa6de19
Thomas <thomaskthomas25003@gmail.com>
2026-07-14 23:34:07 +0530

    added downsizing pixels and converting to array and flattening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import numpy as np
 #img = Image.open(r"COLOR_GEN\image.jpg")
 img = Image.open(r"image.jpg")
 rgb_img = img.convert('RGB')
@@ -13,3 +13,7 @@ rgb_img.thumbnail((244,244), Image.Resampling.LANCZOS)
 rgb_img.show()
 
 #convert to array
+rgb_array=np.array(rgb_img)
+print(rgb_array)
+#flatten
+pixels=rgb_array.reshape(-1,3)
